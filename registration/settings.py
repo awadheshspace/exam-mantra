a737a8cbb1c29ebb3079700ad3cb899127f2aa44
awadheshspace <awadhesh061106@gmail.com>
2025-03-14 23:25:54 +0530

    Update settings.py

--- registration/settings.py
+++ registration/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xkcnnjl93la&ys!)9(++5(yrokasoehj(mx@9ahf$4d8c3+zb5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
